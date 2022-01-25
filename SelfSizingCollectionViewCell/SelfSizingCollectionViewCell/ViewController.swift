580e77586902853711d39e7364fdd9a9386dc49e
angel zambrano <angeldzambrano99@gmail.com>
2022-01-24 23:12:26 -0800

    got rid of storyboard

---
+++ SelfSizingCollectionViewCell/SelfSizingCollectionViewCell/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SelfSizingCollectionViewCell
+//
+//  Created by Angel Zambrano on 1/24/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+//        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+    }
+
+
+}
+
